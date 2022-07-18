9593d6312103bd3325a44c69fda5195be27a67e1
Caroline Rives <car.rives@gmail.com>
2022-07-18 19:22:47 -0400

    Lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
-import java.util.ArrayList;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+
+    /*
+    Given an array of numbers, find all the numbers that are greater than 25.
+
+    Write the method getNums that takes in an array of integers and return an ArrayList
+    containing the numbers that are greater than 25.
+     */
     public static void main(String[] args) {
         int[] numbers = { 1, 4, 5, 2, 34, 36, 5, 4, 23, 45, 6, 8, 56, 34, 37 };
         ArrayList<Integer> res = getNums(numbers);
@@ -9,7 +16,16 @@ public class Main {
     }
 
     public static ArrayList<Integer> getNums(int[] nums) {
-        // your code here
-        return null;
+        List<Integer> numList = Arrays.stream(nums)     // IntStream
+                                      .boxed()          // Stream<Integer>
+                                      .collect(Collectors.toList());
+
+        TreeSet<Integer> treeSetNums = new TreeSet<>();
+        numList.forEach((num) -> treeSetNums.add(num));
+        SortedSet<Integer> sortedSet = treeSetNums.tailSet(25);
+
+        ArrayList<Integer> newList = new ArrayList<>(sortedSet);
+
+        return newList;
     }
 }
